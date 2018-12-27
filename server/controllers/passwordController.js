850c2f20c0e9e41d6c3e4e38e1553901da0f7db3
agnyreza <agny@supersoccer.co.id>
2018-12-27 17:00:33 +0700

    password manager controller

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -24,6 +24,126 @@ module.exports = {
         })
     },
     readByUserId: (req, res) => {
-        
-    }
+        Password.findById({userid : req.body.userid})
+        .sort({updatedAt: -1})
+        .exec()
+        .then(userPassword => {
+            if (userPassword.length) {
+                res.status(200).json({
+                    message: `Find password data success`,
+                    data: userPassword
+                })
+                } else {
+                res.status(202).json({
+                    message: `No data found !`,
+                    data: []
+                })
+                }
+            })
+            .catch((err) => {
+                res.status(202).json({
+                message: `No data found ! ${err}`,
+                data: {}
+                })
+        })
+    },
+    readById: (req, res) => {
+        Password.findById(req.params.id)
+          .exec()
+          .then((password) => {
+            if (password) {
+              res.status(200).json({
+                message: `Get password success`,
+                data: password
+              })
+            } else {
+              res.status(202).json({
+                message: `Get password failed`,
+                data: {}
+              })
+            }
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message: `Error while getting password ${err}`,
+              data: {}
+            })
+          })
+      },
+      
+    update: (req,res)=>{
+        Password.findByIdAndUpdate(req.params.id,{
+            url: req.body.url,
+            username: req.body.username,
+            password: req.body.password
+        },{new: true}, (err, updatedPassword)=>{
+            if (err) {
+                res.status(500).json({
+                  message: `Failed to update password data ${err}`,
+                  data: {}
+                })
+            } else {
+                res.status(200).json({
+                    message: `Update data success`,
+                    data: updatedPassword
+                })
+            }
+        })
+    },
+    delete : (req,res)=>{
+        Password.findByIdAndRemove(req.params.id, (err, deletedPassword) => {
+            if (err) {
+                res.status(500).json({
+                message: `Failed to delete password data ${err}`,
+                data: {}
+                })
+            } else {
+                res.status(200).json({
+                message: `Delete password success`,
+                data: deletedPassword
+                })
+            }
+        })
+    },
+    showPassword:(req,res)=>{
+        User.findOne({
+            $or: [
+                { username: req.body.username_email },
+                { email: req.body.username_email }
+            ]
+        })
+        .exec()
+        .then(userData=>{
+            if(userData){
+                let passwordCheck = bcrypt.compareSync(req.body.password, userData.password)
+                if(passwordCheck){
+                    Password.findById({
+                        _id:req.body.password_id
+                    })
+                    .exec()
+                    .then(password=>{
+                        res.status(200).json({
+                            message: 'show password success',
+                            data: password.password
+                        })
+                    })
+                }else{
+                    res.status(202).json({
+                        message: `Password wrong`,
+                        data: null
+                      })
+                }
+            }else{
+                res.status(202).json({
+                    message: `Password wrong`,
+                    data: null
+                  }) 
+            }
+        })
+        .catch(err=>{
+            res.status(500).json({
+                message: `Error occured on getting users data ${err}`
+            })
+        })
+    },
 }
